007ad04ad12fba9922ee2c1735008871662e5970
DingNan <57606685+happyDaynan@users.noreply.github.com>
2022-08-27 14:47:11 +0800

    Create OrderDetail View, Set RouteConfig,Rename MealOrderController

---
+++ MealOrder_Project/Controllers/MealOrderController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MealOrder_Project.Controllers
+{
+    public class MealOrderController : Controller
+    {
+        // Orders Detail
+        public ActionResult OrdersDetail()
+        {
+            return View();
+        }
+
+        // Create Orders
+        public ActionResult CreateOrders()
+        {
+            return View();
+        }
+
+       
+    }
+}
