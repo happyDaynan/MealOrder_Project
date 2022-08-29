75d9f7214ee853f6c81cff6e9e361a38cc576bf3
DingNan <57606685+happyDaynan@users.noreply.github.com>
2022-08-29 20:06:34 +0800

    Set Category and Type two SelectListItem

--- MealOrder_Project/Tools/GetSelectListItem.cs
+++ MealOrder_Project/Tools/GetSelectListItem.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Mvc;
 using MealOrder_Project.Models;
 
@@ -10,6 +7,63 @@ namespace MealOrder_Project.Tools
 {
     public class GetSelectListItem
     {
-        
+        public static List<SelectListItem> Category()
+        {
+            using (MyCompanyEntities CompanyEntities = new MyCompanyEntities())
+            {
+                var _CategoryList = CompanyEntities.MealCategory_Detail.ToArray();
+
+                List<SelectListItem> _CategorySelectListItem = new List<SelectListItem>();
+
+                foreach (var item in _CategoryList)
+                {
+                    _CategorySelectListItem.Add(new SelectListItem{ 
+                            Text = item.Category,
+                            Value = item.Id.ToString()                    
+                    });
+                }
+
+                _CategorySelectListItem.Add(new SelectListItem
+                {
+                    Text = "--請選擇--",
+                    Value = null,
+                    Selected = true
+                });
+
+                return _CategorySelectListItem;
+
+            }
+
+        }
+
+
+        public static List<SelectListItem> Type()
+        {
+            using (MyCompanyEntities CompanyEntities = new MyCompanyEntities())
+            {
+                var _TypeList = CompanyEntities.MealType_Detail.ToArray();
+
+                List<SelectListItem> _TypeSelectListItem = new List<SelectListItem>();
+
+                foreach(var item in _TypeList)
+                {
+                    _TypeSelectListItem.Add(new SelectListItem { 
+                        
+                        Text = item.Name,
+                        Value = item.Id.ToString()
+                    
+                    });
+                }
+
+                _TypeSelectListItem.Add(new SelectListItem
+                {
+                    Text = "--請選擇--",
+                    Value = null,
+                    Selected = true
+                });
+
+                return _TypeSelectListItem;
+            }
+        }
     }
 }
